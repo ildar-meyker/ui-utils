c17eb1ca3ca4b7d76cc6d353d8fdcfe82ba2f8c6
Ildar Meyker <ildar.meyker@gmail.com>
2026-01-14 17:41:11 +0300

    Update.

--- dist/LessMore.js
+++ dist/LessMore.js
@@ -1,27 +1,27 @@
 export class LessMore {
     constructor(btnEl, options = {}) {
-        this.initializedClass = "lessmore-initialized";
+        this.initializedClass = 'lessmore-initialized';
         // Обработчик клика
         this.handleButtonClick = (event) => {
             event.preventDefault();
             this.toggle();
         };
         if (!btnEl) {
-            throw new Error("LessMore: btnEl is required");
+            throw new Error('LessMore: btnEl is required');
         }
         this.btnEl = btnEl;
         this.options = {
-            btnOpenedClass: "opened",
-            listOpenedClass: "opened",
+            btnOpenedClass: 'opened',
+            listOpenedClass: 'opened',
             ...options,
         };
         const listSelector = btnEl.hash || btnEl.dataset.target;
         if (!listSelector) {
-            throw new Error("LessMore: btn has not list selector");
+            throw new Error('LessMore: btn has not list selector');
         }
         const listEl = document.querySelector(listSelector);
         if (!listEl) {
-            throw new Error("LessMore: listEl not found");
+            throw new Error('LessMore: listEl not found');
         }
         this.listEl = listEl;
         // Проверяем, есть ли уже созданный экземпляр
@@ -35,7 +35,7 @@ export class LessMore {
     // Инициализация кнопки
     init() {
         this.btnEl.classList.add(this.initializedClass);
-        this.btnEl.addEventListener("click", this.handleButtonClick);
+        this.btnEl.addEventListener('click', this.handleButtonClick);
     }
     // Переключить состояние
     toggle() {
@@ -63,7 +63,7 @@ export class LessMore {
     // Удалить экземпляр и обработчик
     destroy() {
         this.btnEl.classList.remove(this.initializedClass);
-        this.btnEl.removeEventListener("click", this.handleButtonClick);
+        this.btnEl.removeEventListener('click', this.handleButtonClick);
         delete this.btnEl._lessmoreInstance;
     }
 }
